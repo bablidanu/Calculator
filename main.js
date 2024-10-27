c9531fda862e825bc8db03164f57c29f12a0ab28
Babli Danu <158181563+bablidanu@users.noreply.github.com>
2024-10-27 20:45:30 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,56 +2,15 @@ const display = document.getElementById("display");
 
 function calc(event) {
   const value = event.target.value;
-  let currentValue = display.value;
-
-  switch (value) {
-    case "=":
-      try {
-        display.value = calculateExpression(currentValue);
-      } catch (error) {
-        display.value = "Error";
-      }
-      break;
-    case "C":
-      display.value = 0;
-      break;
-    default:
-      if (currentValue === "0") {
-        display.value = value;
-      } else {
-        display.value += value;
-      }
-  }
-}
-
-function calculateExpression(expression) {
-  const operators = ["+", "-", "*", "/"];
-  let result = 0;
-  let operator = "+";
-
-  for (let i = 0; i < expression.length; i++) {
-    const char = expression[i];
-
-    if (operators.includes(char)) {
-      operator = char;
-    } else if (!isNaN(char)) {
-      const num = parseFloat(char);
-      switch (operator) {
-        case "+":
-          result += num;
-          break;
-        case "-":
-          result -= num;
-          break;
-        case "*":
-          result *= num;
-          break;
-        case "/":
-          result /= num;
-          break;
-      }
+  if (value === "=") {
+    try {
+      display.value = eval(display.value);
+    } catch (error) {
+      display.value = "Error";
     }
+  } else if (value === "C") {
+    display.value = "0";
+  } else {
+    display.value = display.value === "0" ? value : display.value + value;
   }
-
-  return result;
 }
