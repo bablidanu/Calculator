20c0ec73c0f964e5d662c8fc7afb666cf2024b78
bablidanu <bablidanu05@gmail.com>
2024-11-25 20:47:56 +0530

    updated files

--- main.js
+++ main.js
@@ -2,58 +2,15 @@ const display = document.getElementById("display");
 
 function calc(event) {
   const value = event.target.value;
-  let currentValue = display.value;
-
-  switch (value) {
-    case "=":
-      try {
-        display.value = calculateExpression(currentValue);
-      } catch (error) {
-        display.value = "Error";
-      }
-      break;
-    case "C":
-      display.value = 0;
-      break;
-    default:
-      if (currentValue === "0") {
-        display.value = value;
-      } else {
-        display.value += value;
-      }
+  if (value === "=") {
+    display.value = eval(display.value);
+  } else if (value === "AC") {
+    display.value = 0;
+  } else if (value === "DEL") {
+    display.value = display.value.toString().slice(0, -1);
+  } else if (display.value === "0") {
+    display.value = event.target.value;
+  } else {
+    display.value += event.target.value;
   }
-}
-
-function calculateExpression(expression) {
-  // You can use a library like mathjs or implement your own expression parser
-  // For simplicity, we'll use a basic parser here
-  const operators = ["+", "-", "*", "/"];
-  let result = 0;
-  let operator = "+";
-
-  for (let i = 0; i < expression.length; i++) {
-    const char = expression[i];
-
-    if (operators.includes(char)) {
-      operator = char;
-    } else if (!isNaN(char)) {
-      const num = parseFloat(char);
-      switch (operator) {
-        case "+":
-          result += num;
-          break;
-        case "-":
-          result -= num;
-          break;
-        case "*":
-          result *= num;
-          break;
-        case "/":
-          result /= num;
-          break;
-      }
-    }
-  }
-
-  return result;
 }
